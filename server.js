62483c917c6435a4b0b9d2d31f79e021ec7cc294
Jakelake84 <Lukhanyonomana84@gmail.com>
2026-07-20 03:52:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 // ============================================================
 // 🔥 BOT CONFIG
 // ============================================================
-const BOT_TOKEN = '8950107483:AAGtvDaNSXEA-fULAPn86B6r5jCEn2fEM-A';
+const BOT_TOKEN = '8604504350:AAEEYDta9iHgqaGdgWT8N1h13YpMZb_gbYg';
 const ADMIN_ID = '8595999663';
 
 // ============================================================
